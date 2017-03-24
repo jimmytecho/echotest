7f5013595dcf0e9c0efdc3318df909d7d128e718
jimmytecho <marrch30@gmail.com>
2017-03-24 16:33:14 -0700

    a

--- index.js
+++ index.js
@@ -72,31 +72,37 @@ target: target...
 
 */
 
+source = 'en'
+target = 'fr'
+
 app.post('/webhook/', function (req, res) {
     
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
-        if (event.message) {
-            saymyname(sender)
-
-        }
         if (event.message && event.message.text) {
             text = event.message.text
-            input = text
-            translate({
-                text: input,
-                source: 'en',
-                target: 'fr'
-            }, function (result) {
-                console.log(result);
-                final = String(result);
-            });
-            startprocess()
-            setTimeout(function () {
-                clearTimeout(start);
-            }, 4000)
+            if (text = "xx") {
+                temporary = source
+                source = target
+                target = temporary
+                reply_reversed(sender)
+            }
+            else{
+                translate({
+                    text: text,
+                    source: source,
+                    target: target
+                }, function (result) {
+                    console.log(result);
+                    final = String(result);
+                });
+                startprocess()
+                setTimeout(function () {
+                    clearTimeout(start);
+                }, 4000)
+            }   
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
@@ -107,40 +113,15 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-function saymyname(sender, input) {
-    messageData = {
-        text: sender
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: token },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: messageData,
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-
-
-
 var token = "EAAalYrs1VC8BACEbr72Tj5G8GKMZBfeZAr4iSZCtVZCSVrEZCoKoEOaqZBEPKVF8ekzqmd79Nwp5TdZC8Ud0VaLRfPmWCQlnVvcG9IjUy0AVBh0eAGpxr96Ewq2ON3rB84OTYuaOKGa0saHHflIZC0pnO7g9pSbEvdJ3zLJPvBx7GUabcVn8oxxR"
 function startprocess() {
     start = setTimeout(function () {
-        sendTranslation(sender, text.substring(0, 200));
+        sendTranslation(sender);
     }, 3000);
 }
 
-//all messages
 
-function sendTranslation(sender, input) {
+function sendTranslation(sender) {
     messageData = {
         text: final
     }
@@ -161,3 +142,25 @@ function sendTranslation(sender, input) {
     })
 }
 
+reply_reversed(sender){
+    messageData = {
+        text: "language has reversed!"
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+
+
+}
